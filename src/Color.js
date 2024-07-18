be1aa82e4d334c555a138bc537f0222e991b1c15
Shailesh <shaileshruns1995@gmail.com>
2024-07-18 17:36:07 +0530

    cgng

--- src/Color.js
+++ src/Color.js
@@ -5,7 +5,8 @@ import Actref from "./actref";
 const Color = ()=>{
    const [color,setcolor] = useState("red");
    const cref = useRef("yellow");
-
+   const [astate,setastate] = useState(99);
+     const aref=useRef(99);
    function refcng(){ console.log("ggggggggggggggggggggg")
      setTimeout(()=>{
           cref.current.classList.toggle("clmr");
@@ -15,7 +16,23 @@ const Color = ()=>{
    let b=[5,6,7,8];
    let aa=[...a,...b];
    console.log(aa);
+
+  const handlestate =()=>{
+      setastate(prev=>prev+1)
+      
+      aref.current+=1;
+      console.log(aref.current);
+  }
+
+  const apicall = ()=>{
+     setTimeout(()=>{
+          console.log(aref.current);
+     },5000)
+  }
     return (<div>
+     <button onClick={apicall}>web api call</button>
+     <button  onClick={handlestate}>active state</button>
+     <h1>{aref.current}</h1>
           <Actref aref={cref}/>
          <h1 style={{color:color}} ref={cref}>hey i am {color}</h1>
          <button onClick={()=>{cref.current="purple";
